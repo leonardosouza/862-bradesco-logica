698c7bedbdf475bab4ace0e5276a6f0b8ca56150
Leonardo Souza <leonardo.souza@luizalabs.com>
2022-05-02 18:41:10 -0300

    aula 02

---
+++ aula-02/IndiceMassaCorporal.java
@@ -0,0 +1,18 @@
+public class IndiceMassaCorporal {
+  public static void main(String[] args) {
+    
+    // Armazenar Dados (Variáveis)
+    // tipo chave = valor;
+    int peso = 99;
+    float altura = 1.88F;
+    float imc = peso / (altura * altura);
+
+    // Executar Tarefas
+    // comando();
+    System.out.println("Peso: " + peso);
+    System.out.println("Altura: " + altura);
+    System.out.println("IMC: " + imc);
+
+  }
+  
+}
